8bd839454ee11d060810e01799473d0b4ec7ff13
shirodkarpushkar <pushkars@winjit.com>
2020-05-18 18:09:26 +0530

    timestamp moment

--- time.js
+++ time.js
@@ -1,1 +1,11 @@
-var moment  = require('moment')
+var moment = require('moment')
+var now = moment()
+console.log("now: ", now.format("MMM Do YYYY h:mma"));
+console.log("now: ", now.format("X"));
+console.log("now: ", now.format("x"));
+
+
+var timestamp = 1589805055666;
+var timestampMoment = moment.utc(timestamp)
+
+console.log("timestampMoment", timestampMoment.format("h:mma"));
