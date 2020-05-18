7653a596400bf3519b293e914303b434bc0bcc18
shirodkarpushkar <pushkars@winjit.com>
2020-05-18 10:45:35 +0530

    intialised socket.io application

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 var express = require('express')
 var app = express()
 var http = require('http').Server(app)
-
 var PORT = process.env.PORT || 3000
+
+app.use(express.static(__dirname + '/public'))
+http.listen(PORT, () => {
+    
+    console.log('Server Started at PORT:',PORT)
+})
