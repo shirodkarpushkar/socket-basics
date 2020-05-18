ac3ac46f8a9f6d979458693d6c44917c757796ff
shirodkarpushkar <pushkars@winjit.com>
2020-05-18 11:06:25 +0530

    added configuration for socket

--- server.js
+++ server.js
@@ -2,8 +2,17 @@ var express = require('express')
 var app = express()
 var http = require('http').Server(app)
 var PORT = process.env.PORT || 3000
+var io = require('socket.io')(http)
 
 app.use(express.static(__dirname + '/public'))
+
+io.on('connection', () => {
+    console.log("user conected on socket")
+})
+
+
+
+
 http.listen(PORT, () => {
     
     console.log('Server Started at PORT:',PORT)
